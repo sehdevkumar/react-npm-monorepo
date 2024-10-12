7914bf5491064b0902eaf7cf8fc15c7bef76f79d
sehdevkumar <kumarsehdev600@gmail.com>
2024-10-12 18:22:09 +0530

    common shared utils can consume into the other packages , like native and reacr web

--- packages/react-web/src/components/Button/Button.tsx
+++ packages/react-web/src/components/Button/Button.tsx
@@ -1,15 +1,26 @@
+import { upperCase } from "@shared/utils";
 import React, { FC, ReactNode } from "react"
-
 interface Props{
     children: ReactNode | ReactNode[]
 }
 
 const Button:FC<Props> = ({children}) => {
+ 
+
   return (
-    <button style={{
-        padding: '1em'
-    }} >{children}</button>
-  )
+    <>
+      <button
+        style={{
+          padding: "1em",
+        }}
+      >
+        {children}
+      </button>
+
+      <p>{upperCase("w")}</p>
+
+    </>
+  );
 }
 
 export default Button
