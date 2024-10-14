d3845a2735d0f832504d068a1a0ebd517eca5c47
sehdev <sehdev@atai.ai>
2024-10-14 11:10:58 +0530

    native test application setup completed

---
+++ packages/react-web-test-app/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from 'react-web'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+       <Button >
+          <>Hello World</>
+       </Button>
+    </div>
+  )
+}
+
+export default App
